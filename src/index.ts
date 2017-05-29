979f3afeafb6d46283883956b47a093a482d1676
Jesse Linthorst <jesse.linthorst@gmail.com>
2017-05-29 22:27:15 +0200

    Update imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import 'ts-helpers';
 import { default as _export } from './lib/DeviceStateTracker';
+export { default as DeviceStateEvent } from './lib/DeviceStateEvent';
+export { default as IMediaQuery } from './lib/IMediaQuery';
 
 export default _export;
