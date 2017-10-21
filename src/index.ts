f77244b8c8fe27a652fe13e282f18517f9f21e9a
Jesse Linthorst <jesse.linthorst@gmail.com>
2017-10-21 17:30:10 +0200

    Remove deprecated ts-helpers
    Since TypeScript 2.1 ts-helpers is deprecated (https://github.com/ngParty/ts-helpers).

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import 'ts-helpers';
 import { default as _export } from './lib/DeviceStateTracker';
 export { default as DeviceStateEvent } from './lib/DeviceStateEvent';
 export { default as IDeviceStateConfig } from './lib/IDeviceStateConfig';
